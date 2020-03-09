44bfd8c8c92c91197ff6beb57b72ac8e42f18e81
Azmary-mahi <azmary.mahi@gmail.com>
2020-03-10 01:20:43 +0600

    part7

---
+++ app/src/main/java/com/example/myapplication/bmi.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class bmi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi);
+    }
+
+    public void calculateBMI(View view) {
+    }
+}
