5c784493ba6fc2ff8a92b2cc39b912904eab8306
Kona-Halder <61112889+Kona-Halder@users.noreply.github.com>
2020-03-09 20:43:06 +0600

    main activity update code

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent splash = new Intent(MainActivity.this,splash.class);
+                startActivity(splash);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+
+
+}
+}
+
+
